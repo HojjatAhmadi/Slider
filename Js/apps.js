b9e6fcae2a680976b1b6f8d723bd743fc01b4d30
HojjatAhmadi <maleheabase@gmail.com>
2023-09-09 14:20:50 -0700

    java update

--- Js/apps.js
+++ Js/apps.js
@@ -5,15 +5,42 @@ const LeftButton = document.querySelector('#left')
 
 let conditionMoseClick = false , prevScreenX , prevScrollLeft ;
 
+function RemoveRight(){
+    if (Sliderbg.scrollLeft == 1047){
+        RightButton.style.opacity = 0;
+    }
+    else{
+        RightButton.style.opacity = 1;
+    }
+}
+
+function RemoveLeft(){
+    if (Sliderbg.scrollLeft == 0){
+        LeftButton.style.opacity = 0;
+    }
+    else{
+        LeftButton.style.opacity = 1;
+    }
+}
+
+function RemoveDisplayButton(id) {
+    if (id == 'right'){
+        RemoveRight()
+    }
+    else{
+        RemoveLeft()
+    }
+}
+
 Sliderbg.addEventListener('mousedown', (e) => {
     conditionMoseClick = true ;
     prevScreenX = e.screenX;
     prevScrollLeft = Sliderbg.scrollLeft;
+    console.log(prevScrollLeft)
 })
 
 Sliderbg.addEventListener('mouseup', () => {
      conditionMoseClick = false ;
-
 })
 
 Sliderbg.addEventListener('mousemove', (e) => {
@@ -21,13 +48,52 @@ Sliderbg.addEventListener('mousemove', (e) => {
         let pos = e.pageX - prevScreenX;
         Sliderbg.scrollLeft = prevScrollLeft - pos ;
         e.preventDefault() ;
+        Sliderbg.className = 'drag'
+        if (Sliderbg.scrollLeft == 0 ){
+            RemoveDisplayButton('left')
+        }
+        else{
+            RemoveDisplayButton('left')
+        }
+        if (Sliderbg.scrollLeft == 1047){
+            RemoveDisplayButton('right')
+        }
+        else{
+            RemoveDisplayButton('right')
+        }
     }
 })
 
 RightButton.addEventListener('click', () => {
-    Sliderbg.scrollLeft += ImageSlider.offsetWidth + 14
+    Sliderbg.scrollLeft += ImageSlider.offsetWidth + 30
+    Sliderbg.className = 'slide'
+    if (Sliderbg.scrollLeft == 0 ){
+        RemoveDisplayButton('left')
+    }
+    else{
+        RemoveDisplayButton('left')
+    }
+    if (Sliderbg.scrollLeft == 1047){
+        RemoveDisplayButton('right')
+    }
+    else{
+        RemoveDisplayButton('right')
+    }
 })
 
 LeftButton.addEventListener('click', () => {
-    Sliderbg.scrollLeft -= ImageSlider.offsetWidth + 14
+    Sliderbg.scrollLeft -= ImageSlider.offsetWidth + 30
+    Sliderbg.className = 'slide'
+    if (Sliderbg.scrollLeft == 0 ){
+        RemoveDisplayButton('left')
+    }
+    else{
+        RemoveDisplayButton('left')
+    }
+    if (Sliderbg.scrollLeft == 1047){
+        RemoveDisplayButton('right')
+    }
+    else{
+        RemoveDisplayButton('right')
+    }
 })
